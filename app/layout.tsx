d12e372adc3926191ada761a85f7183ec3b0a9bc
samielectricalservices-hash <samielectricalservices@gmail.com>
2026-07-23 18:02:52 +0300

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,15 @@ import type { Metadata } from "next";
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
-  title: "MSMS",
-  description: "Motor Service Management System"
+  title: "Sami Electrical MSMS",
+  description: "Motor Service Management System",
+  verification: {
+    google: "1jWMcXY10rg8YjBmBEfhznQTrNChtFz_zwt6N3MGhww"
+  },
+  icons: {
+    icon: "logo1.jpg",
+    apple: "logo1.jpg"
+  }
 };
 
 export default function RootLayout({
